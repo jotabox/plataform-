759691f02e08117874170f43d2d322a0fa0d9c5e
jotabox <jotapaulo.box@gmail.com>
2023-06-04 17:40:07 -0300

    implementaçoes de wall slide
    foi implementado o wall slide , wall jump , observaçoes , ajustar a velocidade do pulo , impedir a corrida no ar ,

--- plataforma/Assets/Scripts/Player/PlayerControll.cs
+++ plataforma/Assets/Scripts/Player/PlayerControll.cs
@@ -12,10 +12,17 @@ public class PlayerControll : MonoBehaviour
 
     Rigidbody2D rigi;
     [SerializeField] private float speedX;
-   public float forceY;
+    [SerializeField] private float forceY;
+    [SerializeField] private float wallJumpForceY;
+    [SerializeField] private float runX;
+    [SerializeField] private float wallSlideForce;
     [SerializeField] private bool isJumping = false;
+    [SerializeField] private bool isRunning = false;
+    [SerializeField] private bool isWallSlide;
     [SerializeField] private LayerMask layerMask;
     private BoxCollider2D boxColider;
+    public Transform pointWall;
+
 
 
 
@@ -34,20 +41,42 @@ public class PlayerControll : MonoBehaviour
         Move();
         Jump();
         doubleJump();
+        wallSlide();
+        slideWallJump();
     }
 
 
-    private void Move()
+    private float Horizontal()
     {
         float move = Input.GetAxisRaw("Horizontal");
-        rigi.velocity = new Vector2(move * speedX, rigi.velocity.y);
+        return move;
 
-        if (move > 0)
+    }
+
+    private void Move()
+    {
+
+        // identifica se o player esta correndo
+        if ((Horizontal() > 0 || Horizontal() < 0) && Input.GetButton("Fire1"))
+        {
+            isRunning = true;
+            rigi.velocity = new Vector2(Horizontal() * runX, rigi.velocity.y);
+
+        }
+        else
+        {
+            isRunning = false;
+            rigi.velocity = new Vector2(Horizontal() * speedX, rigi.velocity.y);
+
+        }
+
+
+        if (Horizontal() > 0)
         {
             transform.eulerAngles = new Vector3(0, 0, 0);
         }
 
-        if (move < 0)
+        if (Horizontal() < 0)
         {
             transform.eulerAngles = new Vector3(0, 180, 0);
         }
@@ -59,9 +88,22 @@ public class PlayerControll : MonoBehaviour
 
         if (Input.GetButtonDown("Jump") && onGround())
         {
-            rigi.AddForce(Vector2.up * forceY, ForceMode2D.Impulse);
-            //rigi.velocity = Vector2.up * forceY;
-            isJumping = true;
+            if (!isRunning)
+            {
+                rigi.AddForce(Vector2.up * forceY, ForceMode2D.Impulse);
+                //rigi.velocity = Vector2.up * forceY;
+                isJumping = true;
+                isRunning = false;
+
+
+            }
+            else
+            {            
+                rigi.AddForce(Vector2.up * (forceY + 2), ForceMode2D.Impulse);
+                isJumping = true;
+
+
+            }
         }
     }
 
@@ -72,6 +114,21 @@ public class PlayerControll : MonoBehaviour
             rigi.AddForce(Vector2.up * forceY, ForceMode2D.Impulse);
             //rigi.velocity = Vector2.up * forceY;
             isJumping = false;
+            isRunning = false;
+
+        }
+    }
+
+    private void slideWallJump()
+    {
+        if (isWallSlide && Input.GetButtonDown("Jump"))
+        {
+            //rigi.AddForce(Vector2.up * wallJumpForceY, ForceMode2D.Impulse);
+            rigi.velocity = Vector2.up * wallJumpForceY;
+
+            isJumping = true;
+            isRunning = false;
+
         }
     }
 
@@ -80,6 +137,46 @@ public class PlayerControll : MonoBehaviour
     {
         RaycastHit2D isGrounded = Physics2D.BoxCast(boxColider.bounds.center, boxColider.bounds.size, 0, Vector2.down, 0.1f, layerMask);
         return isGrounded.collider != null;
+    }
+
+    //private void OnDrawGizmos()
+    //{
+    //    Gizmos.color = Color.yellow;
+    //    Gizmos.DrawSphere(pointWall.position, radius);
+    //}
+
+    private bool isWalled()
+    {
+
+
+        bool isWall = Physics2D.OverlapBox(pointWall.position, new Vector2(.8f, 0.88f), 0 , layerMask);
+
+        if(isWall && Horizontal() != 0 && !onGround())
+        {
+            return isWall;
+        }
+        else
+        {
+            return false;
+        }
+
+    }
+
+
+    private void wallSlide()
+    {
+        if (isWalled())
+        {
+            rigi.velocity = new Vector2(rigi.velocity.x, Math.Clamp(rigi.velocity.y, -wallSlideForce, float.MaxValue));
+            isWallSlide = true;
+            isRunning = false;
+
+
+        }
+        else
+        {
+            isWallSlide = false;
+        }
     }
 
 }
